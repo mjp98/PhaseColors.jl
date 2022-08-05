fcf3fa93ab7c9ec5847b6e1083cc16a631e21b22
Matthew Priddin <mjp98@cam.ac.uk>
2022-08-05 16:02:47 +0100

    Add Plots.jl example

--- examples/plots/plots.jl
+++ examples/plots/plots.jl
@@ -1,17 +1,28 @@
 using PhaseColors
 using Plots
 
+# absarg plot
+
 function absarg!(
     x::AbstractVector,
     y::AbstractVector;
-    line_c = nist,
-    line_z=angle.(y),
-    clims=(-π,π),kwargs...)
+    line_c=nist,
+    line_z=canonicalphase.(y),
+    clims=(0, 1), kwargs...)
 
-    plot!(x,abs.(y);line_z,lc=cgrad(line_c,50),clims,kwargs...)
+    plot!(x, abs.(y); line_z, lc=cgrad(line_c, 50), clims, kwargs...)
 
 end
 
-function scatter_reim!(Z::AbstractArray{<:Complex};kwargs...)
-    return scatter!(real.(Z),imag.(Z);kwargs...)
+x = LinRange(0,2π,100)
+y = @. (1+sin(x))*cis(x)
+
+cm = phasecolormaps[:nist]
+
+plot()
+absarg!(x,y;line_c=cm,lw=4,label=:none)
+
+
+function scatter_reim!(Z::AbstractArray{<:Complex}; kwargs...)
+    return scatter!(real.(Z), imag.(Z); kwargs...)
 end
